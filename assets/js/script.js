a3fe396d538aef95f9fdfd56fc926d4f726aaf0b
PetrB <65282144+pettik@users.noreply.github.com>
2024-01-29 16:08:07 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+   // Get all elements with the class "first-row"
+   const firstRows = document.querySelectorAll('.first-row');
+
+   // Iterate through each "first-row" element
+   firstRows.forEach(row => {
+     // Add a click event listener to each "first-row" element
+     row.addEventListener('click', () => {
+       // Toggle the class "open" on the parent ".accordion" element
+       const accordion = row.closest('.accordion');
+       accordion.classList.toggle('open');
+
+       // Toggle the source of the icon image
+       const icon = row.querySelector('.icon img');
+       if (accordion.classList.contains('open')) {
+         icon.src = 'assets/images/icon-minus.svg';
+       } else {
+         icon.src = 'assets/images/icon-plus.svg';
+       }     
+     });
+   });
