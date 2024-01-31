a87639eae12b29240541b0c236172ebe9424537c
pettik <petrbednarski@gmail.com>
2024-01-31 19:58:46 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,28 @@
 const firstRows = document.querySelectorAll('.first-row');
 
-
-
-function init(){
-   const firstAccordion = document.querySelector('#first-accordion');
-   firstAccordion.classList.add('open'); 
-   firstAccordion.querySelector('.icon img').src='assets/images/icon-minus.svg';
+function init() {
+  const firstAccordion = document.querySelector('#first-accordion');
+  firstAccordion.classList.add('open');
+  firstAccordion.querySelector('.icon img').src =
+    'assets/images/icon-minus.svg';
 }
-
-
 
 // Iterate through each "first-row" element
 firstRows.forEach(row => {
-   // Add a click event listener to each "first-row" element
-   row.addEventListener('click', () => {
-      // Toggle the class "open" on the parent ".accordion" element
-      const accordion = row.closest('.accordion');
-      accordion.classList.toggle('open');
+  // Add a click event listener to each "first-row" element
+  row.addEventListener('click', () => {
+    // Toggle the class "open" on the parent ".accordion" element
+    const accordion = row.closest('.accordion');
+    accordion.classList.toggle('open');
 
-      
-
-      // Toggle the source of the icon image
-      const icon = row.querySelector('.icon img');
-      if (accordion.classList.contains('open')) {
-         icon.src = 'assets/images/icon-minus.svg';
-      } else {
-         icon.src = 'assets/images/icon-plus.svg';
-      }
-
-   });
+    // Toggle the source of the icon image
+    const icon = row.querySelector('.icon img');
+    if (accordion.classList.contains('open')) {
+      icon.src = 'assets/images/icon-minus.svg';
+    } else {
+      icon.src = 'assets/images/icon-plus.svg';
+    }
+  });
 });
 
 init();
