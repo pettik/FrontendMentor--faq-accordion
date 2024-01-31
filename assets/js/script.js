1401e0fa9ff3f8bca90569fe50f1c5c8dc19112a
PetrB <65282144+pettik@users.noreply.github.com>
2024-01-31 09:26:03 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,19 @@
-   // Get all elements with the class "first-row"
-   const firstRows = document.querySelectorAll('.first-row');
+const firstRows = document.querySelectorAll('.first-row');
 
-   // Iterate through each "first-row" element
-   firstRows.forEach(row => {
-     // Add a click event listener to each "first-row" element
-     row.addEventListener('click', () => {
-       // Toggle the class "open" on the parent ".accordion" element
-       const accordion = row.closest('.accordion');
-       accordion.classList.toggle('open');
+// Iterate through each "first-row" element
+firstRows.forEach(row => {
+   // Add a click event listener to each "first-row" element
+   row.addEventListener('click', () => {
+      // Toggle the class "open" on the parent ".accordion" element
+      const accordion = row.closest('.accordion');
+      accordion.classList.toggle('open');
 
-       // Toggle the source of the icon image
-       const icon = row.querySelector('.icon img');
-       if (accordion.classList.contains('open')) {
+      // Toggle the source of the icon image
+      const icon = row.querySelector('.icon img');
+      if (accordion.classList.contains('open')) {
          icon.src = 'assets/images/icon-minus.svg';
-       } else {
+      } else {
          icon.src = 'assets/images/icon-plus.svg';
-       }     
-     });
+      }
    });
+});
